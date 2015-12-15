2c9fa7489ce4780cab7e11578d5f668958338940
Uziel Trujillo Colon <uziel.trujillo@gmail.com>
2015-12-15 00:06:48 -0600

    Integrando cada bloque en un slider diferente - Solucionando problemas con CSs - Integrando funcionalidad del md-close
    	modified:   css/core.css
    	deleted:    css/demo.css
    	new file:   css/modal/component.css
    	modified:   css/slideshow.css
    	modified:   index.html
    	modified:   js/exVocacional.js
    	new file:   js/modal/modalEffects.js

--- js/exVocacional.js
+++ js/exVocacional.js
@@ -3,7 +3,7 @@
  * Alumno Zuriel Pineda
  * Asesoria Ing. uziel Trujillo Colón
  * Repositorio de github - https://github.com/utrujillo/testVocational
- * Version .001
+ * Version .003
  */
 (function( $ ){
   
@@ -117,27 +117,20 @@
       siguienteBloque: function( o ){
         console.log('Tus datos registrados hasta ahorita');
         console.log( o.result );
-        if( o.bloque != _.size( o.preguntas ) ){
-          // Ocultamos el bloque(set)
-          methods.ocultarBloque( o );
-          // Incrementamos la variable que nos indica en que bloque(set) estamos
-          // esto lo hacemos porque vamos a cambiar de bloque y queremos que se muestre el siguiente
-          o.bloque += 1;
-          // Reiniciamos la variable pos, para que en el siguiente bloque(set)
-          // las preguntas empiezen a mostrarse desde la primera posicion
-          o.pos = 0;
-          // Mostramos el nuevo bloque
-          methods.mostrarBloque( o );
-          // Restauramos las cajas
-          methods.restaurarCajas( o );
-          // Verificamos en que bloque estamos, para saber que accion tomar
-          methods.verificaBloque( o );
-        }
-        else
-        {
-          $("#myModalAlert2").modal('show');
-          $("#myModal").modal('');
-        }
+        // Ocultamos el bloque(set)
+        methods.ocultarBloque( o );
+        // Incrementamos la variable que nos indica en que bloque(set) estamos
+        // esto lo hacemos porque vamos a cambiar de bloque y queremos que se muestre el siguiente
+        o.bloque += 1;
+        // Reiniciamos la variable pos, para que en el siguiente bloque(set)
+        // las preguntas empiezen a mostrarse desde la primera posicion
+        o.pos = 0;
+        // Mostramos el nuevo bloque
+        methods.mostrarBloque( o );
+        // Restauramos las cajas
+        methods.restaurarCajas( o );
+        // Verificamos en que bloque estamos, para saber que accion tomar
+        methods.verificaBloque( o );
       },
       /**
        * [creaPreguntasRespuestas Metodo utilizado en el bloque tipo 3, que generara preguntas y respuestas dinamicamente]
@@ -167,7 +160,10 @@
         // Insertamos el elemento generado dentro de nuestro objeto
         o.pr.html( toInsert );
       },
-
+      /**
+       * sumaArray Suma los valores de un array pasado por parametro
+       * @return {[Integer] Entero con la sumatoria del array}
+       */
       sumaArray: function(array){
         var suma = 0;
 
@@ -175,6 +171,40 @@
           suma += valor;
         });
         return suma;
+      },
+      /**
+       * showModal Muestra la ventana modal de las instrucciones
+       * @param  {[o]} Variables generales del proyecto
+       * @return {[null]} No retorna nada
+       */
+      showModal: function( o ){
+        
+        o.inst++;
+        var instruccion = "instruccion"+ o.inst;
+        // Asignamos el titulo del modal
+        $('.md-content h3').html( "Bloque "+ o.inst );
+        // Asignamos el cuerpo(mensaje) del modal
+        $('.md-content p').html( o.textoModal[ instruccion ] );
+        // Mostramos el modal
+        $('.instructions').addClass('md-show');
+        
+      },
+      /**
+       * showModalAlert Muestra la ventana modal que se utlizara para alertas, ya sean mensajes de error, mensajes de exito o cualquier otro mensaje que no sea instruccion
+       * @param  {[o]} Variables generales del proyecto
+       * @param  {[String]} nombre del indice de nuestro array asociativo en donde se encontrara la alerta
+       * @return {[none]} No retorna valores
+       */
+      showModalAlert: function( o, msgAlert ){
+        
+        $('.md-content h3').html( "Bloque "+ o.bloque );
+        $('.md-content p').html( o.modalAlerts[ msgAlert ] );
+        $('.modalAlerts').addClass('md-show');
+
+        // Solo se activa la bandera de errores si es que los vamos a mostrar el mensaje de error
+        if( msgAlert == 'msgError' )
+          o.getErrors = true;
+
       },
 
       CalculateResults: function(o){
@@ -195,27 +225,21 @@
               case 4: case 14: case 24: case 34: case 44: case 54:{
                 o.ArtesPlasticas.push( valor );
               } break; 
-
               case 5: case 15: case 25: case 35: case 45: case 55:{
                 o.Musical.push( valor );
               } break; 
-
               case 6: case 16: case 26: case 36: case 46: case 56:{
                 o.OrganizacionYOficina.push( valor );
               } break;
-
               case 7: case 17: case 27: case 37: case 47: case 57:{
                 o.Cientifico.push( valor );
               } break;
-
               case 8: case 18: case 28: case 38: case 48: case 58:{
                 o.CalculoNumerico.push( valor );
               } break;
-
               case 9: case 19: case 29: case 39: case 49: case 59:{
                 o.Mecanico.push( valor );
               } break;
-
               case 10: case 20: case 30: case 40: case 50: case 60:{
                 o.AireLibre.push( valor );
               } break;   
@@ -255,9 +279,7 @@
               color: "#4D5360",
               highlight: "#616774",
               label: "Musical"
-
             },
-
             {
               value: methods.sumaArray( o.OrganizacionYOficina ),
               color:"#11D721",
@@ -312,25 +334,48 @@
      */
     var settings = {
       //Esta variable sirve para hacer debug e indicar al plugin a partir de que bloque deeseamos iniciarokok
-      bloque: 1, 
-      slider: 0,
+      bloque: 1,
       cajaPregunta: $('.cajaPregunta'),
       cajaRespuestas: $('.cajaRespuestas'),
       pr: $('.preguntasRespuestas'),
       pos: 0,
+      minDelay: 500,
+      midDelay: 1000,
+      maxDelay: 1500,
       preguntas: {
-        bloque1: ["1. Atender y cuidar a enfermos.", "2.  Intervenir activamente en discusiones de clase.", "3. Escribir cuentos, crónicas o artículos.", "4. Dibujar y pintar.", "5. Cantar en un orfeón estudiantil.",
-                  "6.  Llevar en orden tus libros y cuadernos.","7. Conocer y estudiar la estructura de las plantas y animales.","8.  Resolver mecanizaciones numéricas.","9. Armar o desarmar objetos mecánicos.","10. Salir de excursión."],
-        bloque2: ["11.  Proteger a los muchachos  menores del grupo.", "12. Ser jefe de una sociedad.", "13.  Leer obras literarias.", "14. Moldear el barro, plastilina o cualquier otro material.", "15.  Escuchar música clásica.",
-                  "16.  Ordenar y clasificar los libros de una biblioteca.","17.  Hacer experimentos en un laboratorio.","18. Resolver problemas de aritmética.","19. Manejar herramienta y maquinaria.","20. Pertenecer a un club de exploradores."],
-        bloque3: ["21.  Ser miembro de una sociedad de ayuda y asistencia.", "22. Dirigir la campaña política de un candidato estudiantil. ", "23.  Hacer versos para una publicación.", "24. Encargarse del decorado de un lugar para un festival.", "25.  Aprender a tocar un instrumento musical.",
-                  "26.  Aprender a escribir en maquina y taquigrafía.","27. Investigar el origen de las costumbre de los pueblos.","28. Llevar las cuentas de una institución.","29.  Construir objetos y muebles.","30.  Trabajar al aire libre fuera de la ciudad."],
-        bloque4: ["31.  Enseñar a leer a los analfabetos.", "32.  Hacer propaganda para la difusión de una idea.", "33. Representar un papel en una obra teatral.", "34.  Idear o diseñar el escudo de un club o sociedad.", "35. Ser miembro de una sociedad musical.",
-                  "36.  Ayudar a calificar pruebas.","37. Estudiar y entender las causas de los movimientos sociales.","38. Explicar a otros como resolver problemas de matemáticas.","39.  Reparar las instalaciones eléctricas;  de gas o de plomería en su casa.","40. Sembrar y planta en una granja durante las vacaciones."],
-        bloque5: ["41.  Ayudar a los compañeros  en sus dificultades y preocupaciones.", "42.  Leer biografías de políticos eminentes.", "43. Participar en un concurso de oratoria.", "44. Diseñar el vestuario para una función teatral.", "45. Leer biografías de músicos eminentes.",
-                  "46.  Encargarse del archivo y de los documentos de una sociedad.","47. Leer revistas y libros científicas.","48. Participar en concursos de matemáticas.","49. Proyectar y dirigir alguna constricción.","50.  Atender animales en un rancho durante las vaciones."],
-        bloque6: ["51.  Funcionario al servicio de las clases humildes.", "52.  Experto en relaciones sociales de una gran empresa.", "53.  Escritor de un periódico o empresa editorial.", "54.  Dibujante profesional de una empresa.", "55.  Concertista de una sinfónica.",
-                  "56.  Técnico organizador de oficinas.","57.  Investigar en un laboratorio.","58. Experto calculista en una institución.","59.  Perito mecánico en un gran taller.","60.  Técnico cuyas actividades se desempeñen fuera de la ciudad."],
+        // bloque1: ["1. Atender y cuidar a enfermos.", "2.  Intervenir activamente en discusiones de clase.", "3. Escribir cuentos, crónicas o artículos.", "4. Dibujar y pintar.", "5. Cantar en un orfeón estudiantil.",
+        //           "6.  Llevar en orden tus libros y cuadernos.","7. Conocer y estudiar la estructura de las plantas y animales.","8.  Resolver mecanizaciones numéricas.","9. Armar o desarmar objetos mecánicos.","10. Salir de excursión."],
+        // bloque2: ["11.  Proteger a los muchachos  menores del grupo.", "12. Ser jefe de una sociedad.", "13.  Leer obras literarias.", "14. Moldear el barro, plastilina o cualquier otro material.", "15.  Escuchar música clásica.",
+        //           "16.  Ordenar y clasificar los libros de una biblioteca.","17.  Hacer experimentos en un laboratorio.","18. Resolver problemas de aritmética.","19. Manejar herramienta y maquinaria.","20. Pertenecer a un club de exploradores."],
+        // bloque3: ["21.  Ser miembro de una sociedad de ayuda y asistencia.", "22. Dirigir la campaña política de un candidato estudiantil. ", "23.  Hacer versos para una publicación.", "24. Encargarse del decorado de un lugar para un festival.", "25.  Aprender a tocar un instrumento musical.",
+        //           "26.  Aprender a escribir en maquina y taquigrafía.","27. Investigar el origen de las costumbre de los pueblos.","28. Llevar las cuentas de una institución.","29.  Construir objetos y muebles.","30.  Trabajar al aire libre fuera de la ciudad."],
+        // bloque4: ["31.  Enseñar a leer a los analfabetos.", "32.  Hacer propaganda para la difusión de una idea.", "33. Representar un papel en una obra teatral.", "34.  Idear o diseñar el escudo de un club o sociedad.", "35. Ser miembro de una sociedad musical.",
+        //           "36.  Ayudar a calificar pruebas.","37. Estudiar y entender las causas de los movimientos sociales.","38. Explicar a otros como resolver problemas de matemáticas.","39.  Reparar las instalaciones eléctricas;  de gas o de plomería en su casa.","40. Sembrar y planta en una granja durante las vacaciones."],
+        // bloque5: ["41.  Ayudar a los compañeros  en sus dificultades y preocupaciones.", "42.  Leer biografías de políticos eminentes.", "43. Participar en un concurso de oratoria.", "44. Diseñar el vestuario para una función teatral.", "45. Leer biografías de músicos eminentes.",
+        //           "46.  Encargarse del archivo y de los documentos de una sociedad.","47. Leer revistas y libros científicas.","48. Participar en concursos de matemáticas.","49. Proyectar y dirigir alguna constricción.","50.  Atender animales en un rancho durante las vaciones."],
+        // bloque6: ["51.  Funcionario al servicio de las clases humildes.", "52.  Experto en relaciones sociales de una gran empresa.", "53.  Escritor de un periódico o empresa editorial.", "54.  Dibujante profesional de una empresa.", "55.  Concertista de una sinfónica.",
+        //           "56.  Técnico organizador de oficinas.","57.  Investigar en un laboratorio.","58. Experto calculista en una institución.","59.  Perito mecánico en un gran taller.","60.  Técnico cuyas actividades se desempeñen fuera de la ciudad."],
+        bloque1: ["1. Atender y cuidar a enfermos."],
+        bloque2: ["11.  Proteger a los muchachos  menores del grupo."],
+        bloque3: ["21.  Ser miembro de una sociedad de ayuda y asistencia."],
+        bloque4: ["31.  Enseñar a leer a los analfabetos."],
+        bloque5: ["41.  Ayudar a los compañeros  en sus dificultades y preocupaciones."],
+        bloque6: ["51.  Funcionario al servicio de las clases humildes."],
+      },
+      inst: 0,
+      getErrors: false,
+      textoModal: {
+        instruccion1: "Lee cuidadosamente la pregunta y selecciona la respuesta que consideres apropiada.",
+        instruccion2: "Lee cuidadosamente la pregunta y arrastra la respuesta que consideres apropiada al recuadro que esta en la parte inferior.",
+        instruccion3: "Lee cuidadosamente las preguntas y contesta acorde a lo que consideres apropiado.",
+
+        instruccion4: "Lee cuidadosamente la pregunta y selecciona la respuesta que consideres apropiada.",
+        instruccion5: "Lee cuidadosamente la pregunta y arrastra la respuesta que consideres apropiada al recuadro que esta en la parte inferior.",
+        instruccion6: "Lee cuidadosamente las preguntas y contesta acorde a lo que consideres apropiado.",
+      },
+      modalAlerts: {
+        msgError: "Cuidado!!<br />Tienes por lo menos una pregunta por contestar, por favor verifica que tengas todo contestado para poder continuar.",
+        msgFinish: "Excelente!!<br /> Haz concluido con el test, a continuación se presentaran los resultados, presiona el boton <b>Cerrar</b> para visualizarlos",
       },
       respuestas: [ 'Me desagrada mucho', 'Me desagrada poco', 'Me es indiferente', 'Me gusta algo', 'Me gusta mucho' ],
       result:[],
@@ -351,9 +396,6 @@
     //======================================
     //      Flujo del Slider
     //======================================
-    // methods.init( o );
-
-    $("#myModal").modal('show');
     /**
      *  Programando el set 1
      *  Este set va a permitir seleccionar la respuesta utilizando el evento click
@@ -382,8 +424,8 @@
       // Por lo tanto tengo que cambiar de bloque(set)
       else
       {
-        methods.siguienteBloque( o );
-        $("#myModal2").modal('show');
+        // Mostramos la ventana modal
+        methods.showModal( o );
       }
     });
 
@@ -419,8 +461,7 @@
           // Por lo tanto tengo que cambiar de bloque(set)
           else
           {
-            methods.siguienteBloque( o );
-            $("#myModal3").modal('show');
+            methods.showModal( o );
           }
         }
       });
@@ -443,10 +484,9 @@
       {
         respuestasTmp.push( parseInt( $(this).val() ) );
       });
-      
+
       // Verifico si dentro de mi arreglo existe por lo menos un 5
       var vacio = $.inArray ( 5 , respuestasTmp );
-      
       // Si el arreglo retorna -1 quiere decir que el elemento buscado no fue encontrado
       // y como el 5 es -Seleccionar- quiere decir que el usuario selecciono por lo menos una opcion
       // de las 5 posibles
@@ -454,27 +494,76 @@
       {
         // Ya que el usuario ha seleccionado por lo menos una opcion
         // agrego el arreglo temporal a mi verdadero arreglo de respuestas
+        o.getErrors = false;
         o.result = $.merge( o.result, respuestasTmp );
-        methods.siguienteBloque( o );
-        $("#myModal").modal('show');
+
+        // Verificamos que no sea el ultimo bloque
+        if( o.bloque != _.size( o.preguntas ) )
+        {
+          methods.showModal( o );
+        }
+        // En caso de ser el ultimo bloque, mostramos la ventana final
+        else
+        {
+          methods.showModalAlert( o, 'msgFinish' );
+        }
+          
       }
       // Si el usuario tiene por lo menos una respuesta no seleccionada
       // entonces no podra avanzar a la siguiente etapa
       else  
       {
-         $("#myModalAlert").modal('show');
+         methods.showModalAlert( o, 'msgError' );
       };
 
-
+    });
+  
+    // Boton para avanzar el slider de manera manual
+    $('.siguienteSlide').click(function(){
+      o.slider._navigate('next');
     });
 
-      $("#ShowResults").click(function(){
+    /**
+    * Funcionamiendo de ventana modal
+    */
+    $('.iniciarTest').click(function(){
+      methods.showModal( o );
+    });
 
+    $('.md-close').click(function(){
+
+      // Limpiando valores de ventana modal
+      $('.md-content h3').html( "" );
+      $('.md-content p').html( "" );
+
+      // Oculto la ventana modal
+      $(this).parents().eq(2).removeClass("md-show");
+
+      // Si es el primer bloque, inicio la actividad
+      if( o.inst == 1  && !o.getErrors )
+      {
+        o.slider._navigate('next');
+        setTimeout(function(){ methods.init( o ); }, o.minDelay);
+      }
+      // Si no es el primero, entonces avanzo al siguiente bloque
+      // Verificamos que no exista un error guardado, esto es por el bloque 3
+      else if( ( o.bloque != _.size( o.preguntas ) ) && !o.getErrors )
+      {
+        console.log(o.bloque+" "+_.size( o.preguntas )+" "+o.getErrors);
+        o.slider._navigate('next');
+        methods.siguienteBloque( o );
+
+      }
+      // Si es el ultimo bloque y no hay errores
+      // Es hora de terminar el test
+      else if( ( o.bloque == _.size( o.preguntas ) ) && !o.getErrors)
+      {
+        o.slider._navigate('next');
         methods.CalculateResults(o);
-        $("#container").html("");
-        methods.showGraphic(o);
-              
-      });
+        setTimeout(function(){ methods.showGraphic( o ); }, o.midDelay);
+      }
+
+    });
 
   }//fin exVocacional function
 
